04264b1669d7784ddf4cd6ea394a3de573e861de
Osher <solimani.osher@gmail.com>
2024-09-14 22:08:22 +0300

    Connect client to server

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import "express-async-errors";
 
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
 import morgan from "morgan";
 import express from "express";
 import * as dotenv from "dotenv";
@@ -8,9 +12,25 @@ dotenv.config();
 
 const app = express();
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.get("/sendEmail", async (req, res) => {
+  res.send("sendEmail");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
+
+app.use("*", (req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
 const port = process.env.PORT || 5100;
 
